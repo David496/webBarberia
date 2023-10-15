a200294d08259e9cbf0634eca95d9b5c3f08617d
Jose <jose_1996_9@hotmail.com>
2023-10-14 19:23:46 -0500

    login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<html lang="es" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg"
+      data-sidebar-image="none">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Iniciar Sesión | SWGRVB</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta content="LARC CONSULTORES CLIENTES" name="description" />
+    <!-- App favicon -->
+    {{-- <link rel="shortcut icon" href="{{ URL::asset('assets/images/racso/Favicon_new_racso.png') }}"> --}}
+
+    <!-- Layout config Js -->
+    <script src="{{ asset('assets/js/layout.js') }}"></script>
+
+    <!-- Bootstrap Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" type="text/css" />
+
+    <!-- Icons Css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/icons.min.css') }}" type="text/css" />
+
+    <!-- App Css-->
+    <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}" type="text/css" />
+
+    <!-- custom Css-->
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.min.css') }}" type="text/css" />
+</head>
+
+<body style="background: linear-gradient(63deg, rgba(8,20,83,1) 0%, rgba(5,10,36,1) 100%);">
+    <div class="auth-page-wrapper pt-5 " >
+
+        <!-- auth page bg -->
+        <div style="position: absolute; left: 0px; top: 0px; overflow: hidden; width: 100%; height: 100%;" id="auth-particles">
+        </div>
+        <!-- auth page content -->
+        <div class="auth-page-content ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center mt-sm-5 mb-4 text-white-50">
+                            <div>
+                                <a href={{ url('login') }} class="d-inline-block auth-logo">
+                                    {{-- <img src="{{ asset('assets/images/racso/NEW_RACSO_blanco.png') }}" alt="" height="70"> --}}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end row -->
+
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card mt-4">
+
+                            <div class="card-body p-4">
+                                <div class="text-center mt-2">
+                                    <p class="text-dark">Bienvenido!</p>
+                                    <h3 class="text-primary fw-bold">Iniciar Sesión</h3>
+                                </div>
+                                <div class="p-2 mt-4">
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label for="username" class="form-label">Doc. Identidad</label>
+                                            <input type="text" name="dni"
+                                                   class="form-control  @error('dni') is-invalid @enderror" autofocus
+                                                   value="{{ old('dni') }}" autocomplete="dni" id="username" required
+                                                   placeholder="Ingrese DNI">
+                                            @error('dni')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <div class="float-end">
+                                                @if (Route::has('password.request'))
+                                                <a href="{{ route('password.request') }}" class="text-muted">¿Olvidaste la
+                                                    contraseña?</a>
+                                                @endif
+                                            </div>
+                                            <label class="form-label" for="password">Contraseña</label>
+                                            <div class="position-relative auth-pass-inputgroup mb-3">
+                                                <input type="password"
+                                                       class="form-control pe-5  password @error('password') invalid @enderror"
+                                                       name="password" required placeholder="Ingrese Contraseña"
+                                                       id="password" autocomplete="new-password">
+                                                <button
+                                                        class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon"
+                                                        type="button" id="password-addon"><i
+                                                       class="ri-eye-fill align-middle"></i></button>
+
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember"
+                                                id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="auth-remember-check">Recuérdame</label>
+                                        </div>
+
+                                        <div class="mt-4">
+                                            <button class="btn btn-success w-100" type="submit" onclick="this.form.submit();this.disabled=true;
+                                            this.innerHTML='<i class=\'bx bx-loader bx-spin\'></i> Cargando';">Iniciar
+                                                Sesión</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- end card body -->
+                        </div>
+                        <!-- end card -->
+                    </div>
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end auth page content -->
+
+        <!-- footer -->
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p class="mb-0 text-muted">&copy;
+                                <script>
+                                    document.write(new Date().getFullYear())
+                                </script> SWGRVB
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end Footer -->
+    </div>
+    <!-- end auth-page-wrapper -->
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+
+    <!-- password-addon init -->
+    <script src="{{ asset('assets/js/pages/password-addon.init.js') }}"></script>
+
+</body>
+
+</html>
+
+
+{{-- @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email"
+                                class="form-control @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                                name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection --}}
